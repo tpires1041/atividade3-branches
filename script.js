63eeabc3483c3bedf216e3a4aba36d05a0b50ed1
Tiago Pires <tiagopires2501@outlook.com>
2024-10-26 22:41:14 -0400

    Input de nome
    Input de nome adicionado

---
+++ script.js
@@ -0,0 +1,8 @@
+let mostrarDados = function () {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+
+    document.getElementById("resultadoNome").innerHTML = nome;
+    document.getElementById("resultadoIdade").innerHTML = idade;
+
+}
